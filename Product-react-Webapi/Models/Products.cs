3b1dfdb7728f7e341ee5926a9ae466c228cc097d
Rohan Phadtare <Rohan.Phadtare@encora.com>
2025-11-21 16:55:22 +0530

    Add project files.

---
+++ Product-react-Webapi/Models/Products.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Product_react_Webapi.Models
+{
+    public class Products
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
